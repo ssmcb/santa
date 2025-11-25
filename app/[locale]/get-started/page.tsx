2ab5acd5a6ba023ec7fddd4e2b67ec1ac629f77c
saulomarchi <saulo.marchi@coinbase.com>
2025-11-25 15:15:57 -0500

    flow works

--- app/[locale]/get-started/page.tsx
+++ app/[locale]/get-started/page.tsx
@@ -1,6 +1,3 @@
-import { redirect } from 'next/navigation';
-import { connectDB } from '@/lib/db/mongodb';
-import { AdminUser } from '@/lib/db/models/AdminUser';
 import { GetStartedForm } from './GetStartedForm';
 
 type GetStartedPageProps = {
@@ -9,15 +6,6 @@ type GetStartedPageProps = {
 
 export default async function GetStartedPage({ params }: GetStartedPageProps) {
   const { locale } = await params;
-
-  // Check if admin user already exists
-  await connectDB();
-  const adminUser = await AdminUser.findOne({ is_registered: true });
-
-  if (adminUser) {
-    // Redirect to login/home if admin already exists
-    redirect(`/${locale}`);
-  }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-zinc-950 p-4">
